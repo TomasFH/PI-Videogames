6cbc1f9a5f5b6b691f6a69e49161f24082b4e25e
Tomas Holgado <creadordecaminos@gmail.com>
2022-07-09 16:16:31 -0300

    Fix: removed redux devtool extension from the reducer. Having done that should allow other browsers to see the app.

--- client/src/store1/index.js
+++ client/src/store1/index.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 
 const store = legacy_createStore(reducer, compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
