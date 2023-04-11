746d810e1e64bac566744dba8f5cc205a0ed9f33
Tomas Holgado <creadordecaminos@gmail.com>
2023-04-11 16:25:48 -0300

    changed port to PORT (variable) on db.js

--- api/index.js
+++ api/index.js
@@ -52,6 +52,6 @@ conn.sync({ force: false }).then(() => {
   createGenreList();
 
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
